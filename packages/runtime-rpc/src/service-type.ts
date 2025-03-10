bfbb18f8127b9c809938cbd15faa90ccb4abcf7d
oott123 <git@public.oott123.com>
2025-03-10 14:50:26 +0800

    feat: add indexable methods to service

--- packages/runtime-rpc/src/service-type.ts
+++ packages/runtime-rpc/src/service-type.ts
@@ -1,9 +1,11 @@
 import {MethodInfo, normalizeMethodInfo, PartialMethodInfo, ServiceInfo} from "./reflection-info";
 import type {JsonValue} from "@protobuf-ts/runtime";
 
+type MethodInfoMap<Methods extends Record<string, true>> = {
+    [key in keyof Methods]: MethodInfo;
+}
 
-
-export class ServiceType implements ServiceInfo {
+export class ServiceType<T extends Record<string, true> = {}> implements ServiceInfo {
 
     /**
      * The protobuf type name of the service, including package name if
@@ -15,17 +17,19 @@ export class ServiceType implements ServiceInfo {
      * Information for each rpc method of the service, in the order of
      * declaration in the source .proto.
      */
-    readonly methods: MethodInfo[];
+    readonly methods: MethodInfo[] & MethodInfoMap<T>;
 
     /**
      * Contains custom service options from the .proto source in JSON format.
      */
     readonly options: JsonOptionsMap;
 
-
     constructor(typeName: string, methods: PartialMethodInfo[], options?: JsonOptionsMap) {
         this.typeName = typeName;
-        this.methods = methods.map(i => normalizeMethodInfo(i, this));
+        this.methods = methods.map(i => normalizeMethodInfo(i, this)) as MethodInfo[] & MethodInfoMap<T>;
+        for (const method of this.methods) {
+            ;(this.methods as any)[method.name] = method;
+        }
         this.options = options ?? {};
     }
 }
